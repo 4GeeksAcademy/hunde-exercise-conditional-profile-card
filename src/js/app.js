647c7ecb73e4188daf0cac6d681d0b2ee162f388
lijdani <166644142+lijdani@users.noreply.github.com>
2024-08-14 13:44:26 +0000

    hunde

--- src/js/app.js
+++ src/js/app.js
@@ -33,9 +33,9 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
+          <h1>Hunde Marga</h1>
+          <h2>Full Stack Web Developer</h2>
+          <h3>Minnneapolis, USA</h3>
           <ul class="position-right">
             <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
             <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
@@ -54,9 +54,11 @@ window.onload = function() {
     // if includeCover is true the algorithm should show the cover image
     includeCover: true,
     // this is the image's url that will be used as a background for the profile cover
-    background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da",
+    background:
+      "https://demo-source.imgix.net/canyon.jpg?fit=crop&w=191&h=153&dpr=1&q=75&auto=format%2Ccompress&cacheID=3434113948",
     // this is the url for the profile avatar
-    avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
+    avatarURL:
+      "https://ca.slack-edge.com/T0BFXMWMV-U06REKTDBAQ-e736ab75dc3f-512",
     // social media bar position (left or right)
     socialMediaPosition: "position-left",
     // social media usernames
